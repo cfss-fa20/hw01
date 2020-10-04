3fd822227697695a605b5ee08fcc33ed206f778a
Kyrie <tongtong@tongtongdeMacBook-Air.local>
2020-10-03 22:55:58 -0400

    I revised something.

--- README.md.R
+++ README.md.R
@@ -1,20 +1,21 @@
 
-  
+==========  
 #hw01
 My name is Aria Huang.
-
-=========Research Interest==========
-  Aria is interested in *diversity policy* and *workplace inequalities*.
-  Aria wants to use _large-scale dataset_ to finish her thesis, so she is taking the computing for social science course.
------------Personal Interest-------------
-    Aria also develops several hobbies in her real life, including:
+##Research Interest
+Aria is interested in *diversity policy* and *workplace inequalities*.
+Aria wants to use _large-scale dataset_ to finish her thesis, so she is taking the computing for social science course.
+###Personal Interest
+Aria also develops several hobbies in her real life, including:
     * Baking.
     * Painting.
     * Watching TV shows.
       - Chinese soap dramas.
       + Korean dramas.
       - Netflix reality show.
-    The favorite show is called Reply 1988. Here is the link : (https://en.wikipedia.org/wiki/Reply_1988).
+    The favorite show is called [Reply 1988](https://en.wikipedia.org/wiki/Reply_1988).
 #images
-    [id]: /Users/tongtong/Downloads/me.jpg "me"
+    ![alt text](/Users/tongtong/Downloads/me.jpg "me")
+    
+
     
